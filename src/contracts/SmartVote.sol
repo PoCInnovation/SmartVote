49cd95c48820272cb1f21a44ad97fdf7faad31ec
Quentin Fringhian <quentin.fringhian@epitech.eu>
2020-12-27 16:42:24 +0100

    feat: subAdministrator default action

--- src/contracts/SmartVote.sol
+++ src/contracts/SmartVote.sol
@@ -2,18 +2,41 @@ pragma solidity ^0.7.4;
 
 contract SmartVote {
     address private administrator;
+    address[10] private subAdministrator;
 
-    modifier isAdministrator() {
+    modifier isNotSubAdministratorNorAdministratorModifier() {
+        require(msg.sender != administrator && !isSubAdministrator(msg.sender));
+        _;
+    }
+    modifier isAdministratorModifier() {
         require(msg.sender == administrator);
         _;
     }
-
-    function setAdministrator(address _newAdministrator) private {
-        administrator = _newAdministrator;
+    modifier isSubAdministratorModifier() {
+        require(isSubAdministrator(msg.sender));
+        _;
     }
-
+   
     function setDefaultAdministrator() public {
         require(administrator == address(0));
         setAdministrator(msg.sender);
+    }
+    function setAdministrator(address _newAdministrator) private {
+        administrator = _newAdministrator;
+    }
+   
+    function isSubAdministrator(address _address) public returns(bool) {
+        require(_address != address(0));
+        for (uint i = 0; len(subAdministrator); i++) {
+            if (subAdministrator[i] == _address) {
+                return true;
+            }
+        }
+        return false;
+    }
+    function setSubAdministrator(uint _indexValue, address _newSubAdministrator) private 
+    isNotSubAdministratorNorAdministratorModifier(_newSubAdministrator) 
+    isAdministratorModifier(msg.sender){
+        subAdministrator[_indexValue] = _newSubAdministrator;
     }
 }
