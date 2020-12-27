262ede1e4d347ec4ec5abb765cefeb5c521b5596
Quentin Fringhian <quentin.fringhian@epitech.eu>
2020-12-27 16:08:21 +0100

    feat: default administrator interaction

--- src/contracts/SmartVote.sol
+++ src/contracts/SmartVote.sol
@@ -1,4 +1,19 @@
 pragma solidity ^0.7.4;
 
 contract SmartVote {
+    address private administrator;
+
+    modifier isAdministrator() {
+        require(msg.sender == administrator);
+        _;
+    }
+
+    function setAdministrator(address _newAdministrator) private {
+        administrator = _newAdministrator;
+    }
+
+    function setDefaultAdministrator() public {
+        require(administrator == address(0));
+        setAdministrator(msg.sender);
+    }
 }
