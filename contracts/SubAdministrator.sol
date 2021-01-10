af5b2f8040fe961eac33a7d017c072860c0b00cd
Quentin Fringhian <quentin.fringhian@epitech.eu>
2021-01-10 22:52:55 +0100

    feat: documentation for the SubAdministrator contract

--- contracts/SubAdministrator.sol
+++ contracts/SubAdministrator.sol
@@ -3,12 +3,28 @@ pragma solidity ^0.7.4;
 
 import "./SmartVote.sol";
 
+/**
+* @title SubAdministrator
+* @dev The SubAdministrator contract is a subcontract to the SmartVote
+* contract. It is there to represent all the interaction with
+* a subadministrator or by a user of subadministrator.
+*/
 contract SubAdministrator is SmartVote {
+    /**
+    * @dev This modifier will tell if a given address represent
+    * one of the subadministrator. It works with the isSubAdministrator
+    * function.
+    */
     modifier onlySubAdministrator(address _address) {
         require(isSubAdministrator(_address));
         _;
     }
 
+    /**
+    * @dev The isSubAdministrator will return a bool, true if the
+    * given address (_address) represent the address of a subadministrator,
+    * and false if he is not.
+    */
     function isSubAdministrator(address _address) public view returns(bool) {
         require(_address != address(0));
         for (uint8 i = 0; i < numberSubAdministrator; i++) {
@@ -18,7 +34,13 @@ contract SubAdministrator is SmartVote {
         }
         return false;
     }
-    function setSubAdministrator(uint _indexValue, address _newSubAdministrator) private 
+
+    /**
+    * @dev The setSubAdministrator function is only callable by the administrator.
+    * It will set the given address (_address) as a subadministrator at the given
+    * position (_indexValue) in the array.
+    */
+    function setSubAdministrator(uint _indexValue, address _newSubAdministrator) private
     onlyOwner {
         require(_indexValue < numberSubAdministrator);
         require(!isSubAdministrator(_newSubAdministrator));
